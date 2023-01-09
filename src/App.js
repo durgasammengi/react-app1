7f7d8c911cc7acba8046b70f3ffec2a73a8898c6
prasad <durgasammengi@gmail.com>
2023-01-09 10:29:03 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 import './App.css';
 import Header from './components/header/header';
 import Main from "./components/main/main";
 import About from "./about";
-import Footer from "./components/footer/footer";
+function Modal({ children, shown, close }) {
+  return shown ? (
+      <div
+        className="modal-content"
+        onClick={e => {
+          e.stopPropagation();
+        }}
+      >
+        <button onClick={close}>Close</button>
+        {children}
+      </div>
+  ) : null;
+}
 
 function App() {
+  const [modalShown, toggleModal] = React.useState(true);
   return (
     <div className="App">
-      {/* <div className="App-img">image</div>
-      <header className="App-header">
-        <h1>It's Prasad Sammengi</h1>
-      </header> */}
-     
+      <Modal
+       shown={modalShown}
+        close={() => {
+          toggleModal(false);
+        }}
+      >
+        <h1>Look! I'm inside the modal!</h1>
+      </Modal>
       <Router>
-      <Header title="꧁༺քʀǟֆǟɖ ֆǟʍʍɛռɢɨ༻꧂"  />
+      <Header title="Prasad Sammengi"  />
       <h6></h6>
+      
         <Routes>
-         
+            <Route index element={<Navigate to="/home" replace />} /> 
             <Route path="/home" element={<Main message="Experienced Java developer with a strong track record of
             delivering successful projects. Proficient in Java 8 and above, with a
             solid understanding of design patterns and object-oriented programming. 
@@ -30,12 +48,12 @@ function App() {
             Seeking a challenging Java development role where I can utilize my skills and continue 
             learning and growing as a developer."       
             note="@ps"/>} />
-            <Route path="about" element={<About about="checking"/>}/>
+            <Route path="/about" element={<About about="checking"/>}/>
       
           </Routes>
       </Router>
-      
     </div>
+    
   );
 }
 
